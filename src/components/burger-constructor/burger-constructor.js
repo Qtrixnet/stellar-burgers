91f0ab10f0392509c075c7113a2467a4a51a4992
Kirill <qtrixnet@yandex.ru>
2021-11-08 22:02:19 +0600

    сверстать компонент конструктора

--- src/components/burger-constructor/burger-constructor.js
+++ src/components/burger-constructor/burger-constructor.js
@@ -7,27 +7,31 @@ export default function BurgerConstructor() {
   const total = composeData.reduce((acc, cur) => acc + cur.price, 0)
 
   return (
-    <ul className={`${burgerConstructorStyle.list} pl-4 pr-4 pt-25`}>
-      {composeData.map((ingredient, idx) => {
-        const topOrBottom = idx === 0 && 'top' || idx === composeData.length - 1 && 'bottom'
-        return <li key={idx} className={burgerConstructorStyle.list_item}>
-          {!topOrBottom && <DragIcon />}
-          <ConstructorElement
-            type={topOrBottom}
-            isLocked={topOrBottom}
-            text={ingredient.name}
-            price={ingredient.price}
-            thumbnail={ingredient.image}
-          />
-        </li>
-      })}
-      <div className="pt-6">
-        <span className="text text_type_digits-medium mr-2">{total}</span>
-        <CurrencyIcon type="primary" />
-        <Button className="ml-10" type="primary" size="medium">
-          Оформить заказ
-        </Button>
-      </div>
-    </ul>
+    <div className="pt-25">
+      <ul className={`${burgerConstructorStyle.list} pl-4 pr-4`}>
+        {composeData.map((ingredient, idx) => {
+          const topOrBottom = idx === 0 && 'top' || idx === composeData.length - 1 && 'bottom'
+          return <li key={idx} className={burgerConstructorStyle.list_item}>
+            {!topOrBottom && <DragIcon />}
+            <ConstructorElement
+              type={topOrBottom}
+              isLocked={topOrBottom}
+              text={ingredient.name}
+              price={ingredient.price}
+              thumbnail={ingredient.image}
+            />
+          </li>
+        })}
+        <div className={`${burgerConstructorStyle.button_container} pt-6`}>
+          <div className='mr-10'>
+            <span className="text text_type_digits-medium mr-2">{total}</span>
+            <CurrencyIcon type="primary" />
+          </div>
+          <Button className="pt-10" type="primary" size="medium">
+            Оформить заказ
+          </Button>
+        </div>
+      </ul>
+    </div>
   );
 };
