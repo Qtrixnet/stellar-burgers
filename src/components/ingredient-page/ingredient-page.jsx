cbf9c9ad687453c03ee2a3b6a7b3aa83d8b77a77
Kirill <qtrixnet@yandex.ru>
2022-02-07 23:05:06 +0600

    создать компонент страницы ингредиента

---
+++ src/components/ingredient-page/ingredient-page.jsx
@@ -0,0 +1,12 @@
+import IngredientPageStyles from './ingredient-page.module.css';
+import IngredientDetails from '../ingredient-details/ingredient-details';
+
+const IngredientPage = () => {
+  return (
+    <section>
+      <IngredientDetails />
+    </section>
+  )
+}
+
+export default IngredientPage;
--- src/components/loader/loader.jsx
+++ src/components/loader/loader.jsx
@@ -1,7 +1,7 @@
 import loaderStyles from './loader.module.css';
 
-const Loader = () => <div className={loaderStyles.loader_container}>
+const Loader = () => (<div className={loaderStyles.loader_container}>
   <div className={loaderStyles.loader}></div>
-</div>
+</div>)
 
 export default Loader;
